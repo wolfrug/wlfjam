5efa0564b0e27fe774e4f7b178b4a265eb921a49
Lassi <lassi.n94@gmail.com>
2022-12-03 15:59:11 +0200

    add movable enemy and checkpoint system

---
+++ wlfjam2022/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.transform.CompareTag("Player")) {
+            collision.transform.GetComponent<Player>().GetHit();
+        }
+    }
+}
