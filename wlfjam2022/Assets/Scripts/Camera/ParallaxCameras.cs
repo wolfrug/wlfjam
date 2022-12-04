184885c2796a402c53ebf63482e7682313a1e79b
pskult <petter.skult@gmail.com>
2022-12-04 04:03:55 +0200

    added player anims, new platforms, removed temp stuff

--- wlfjam2022/Assets/Scripts/Camera/ParallaxCameras.cs
+++ wlfjam2022/Assets/Scripts/Camera/ParallaxCameras.cs
@@ -13,27 +13,42 @@ using UnityEngine;
 [System.Serializable]
 public class ParallaxBackground {
     public Camera parallaxCamera;
+    public string parallaxCameraTag = "ParallaxCamera1";
     public GameObject[] parallaxBackgrounds;
     public float fovDistance = 60f;
 }
 public class ParallaxCameras : MonoBehaviour {
-    public List<ParallaxBackground> m_backgrounds;
 
-    public float m_scaleToFOVRatio = 3f; // how many times larger the fov is to the scale
+    public Camera m_mainCamera;
+    public List<ParallaxBackground> m_backgrounds;
+    public float m_yAdjustment = -32f; // how far down the parallax background object should be placed
+
+    public float m_scaleAdjustment = 0.001f; // to avoid super large objects
 
     // Start is called before the first frame update
     void Start () {
+        if (m_mainCamera == null) {
+            m_mainCamera = Camera.main;
+        }
         foreach (ParallaxBackground bck in m_backgrounds) {
             SetupBackground (bck);
         }
+        transform.position = new Vector3 (transform.position.x, m_yAdjustment, transform.position.z);
     }
 
     void SetupBackground (ParallaxBackground bck) {
-        bck.parallaxCamera.fieldOfView = bck.fovDistance;
-        float ratiodScale = bck.fovDistance / m_scaleToFOVRatio;
-        foreach (GameObject go in bck.parallaxBackgrounds) {
-            go.transform.localScale = new Vector3 (ratiodScale, ratiodScale, ratiodScale);
+        if (bck.parallaxCamera == null) {
+            bck.parallaxCamera = GameObject.FindGameObjectWithTag (bck.parallaxCameraTag).GetComponent<Camera> ();
         }
+        //bck.parallaxCamera.fieldOfView = bck.fovDistance;
+        bck.parallaxCamera.fieldOfView = m_mainCamera.fieldOfView;
+        foreach (GameObject go in bck.parallaxBackgrounds) {
+            go.transform.position = new Vector3 (go.transform.position.x, go.transform.position.y, bck.fovDistance);
+            float distance = (m_mainCamera.transform.position - go.transform.position).magnitude;
+            float size = distance * m_scaleAdjustment * m_mainCamera.fieldOfView;
+            go.transform.localScale = Vector3.one * size;
+        }
+
     }
 
     // Update is called once per frame
