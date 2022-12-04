dd8585364310634ea643b14e343cd531809ddee2
pskult <petter.skult@gmail.com>
2022-12-04 19:13:30 +0200

    added checkpoint visualizers and made dance pad visually show being "filled up"...

--- wlfjam2022/Assets/Scripts/Interactables/Checkpoint.cs
+++ wlfjam2022/Assets/Scripts/Interactables/Checkpoint.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Checkpoint : MonoBehaviour
-{
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.CompareTag("Player")) {
-            collision.GetComponent<Player>().HitCheckPoint(transform);
+[System.Serializable]
+public class CheckpointReachedEvent : UnityEvent<Checkpoint> { }
+
+public class Checkpoint : MonoBehaviour {
+
+    public Cloth m_checkpointCloth;
+    public CheckpointReachedEvent m_checkPointEvent;
+    private void OnTriggerEnter2D (Collider2D collision) {
+        if (collision.CompareTag ("Player")) {
+            collision.GetComponent<Player> ().HitCheckPoint (transform);
+            m_checkPointEvent.Invoke (this);
+            if (m_checkpointCloth != null) {
+                m_checkpointCloth.externalAcceleration = new Vector3 (-80f, 30f, 0f);
+            }
         }
     }
 }
